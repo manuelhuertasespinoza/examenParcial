35fd28b409a4af0c8e6729064f6ccd0ff3648169
manuelhuertasespinoza <manuelhuertasespinoza@gmail.com>
2023-12-03 13:20:22 -0500

    MODALES

--- examenParcial/gestionTienda/models.py
+++ examenParcial/gestionTienda/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class tiendas(models.Model):
+    direccion= models.CharField(max_length=50,blank=True,null=True)
+    provincia= models.CharField(max_length=50,blank=True,null=True)
+    region= models.CharField(max_length=50,blank=True,null=True)
+    fecha_creacion= models.CharField(max_length=50,blank=True,null=True)
+    telefono= models.CharField(max_length=9,blank=True,null=True)
+
+class productos(models.Model):
+    descripcion= models.CharField(max_length=200,blank=True,null=True)
+    codigo= models.CharField(max_length=15,blank=True,null=True)
+    pre_venta= models.CharField(max_length=50,blank=True,null=True)
+    cantidad= models.CharField(max_length=50,blank=True,null=True)
+    tienda_relacionada= models.ForeignKey(tiendas,blank=True,null= True,on_delete=models.SET_NULL)
--- examenParcial/gestionTienda/views.py
+++ examenParcial/gestionTienda/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import tiendas
+from .models import productos
 # Create your views here.
 def productos (request):
     return render(request,'productos.html')
